17e9bee838357bcaeda20238d4fd7b8551f9438d
k57mz876tb-del <k57mz876tb@privaterelay.appleid.com>
2026-09-15 11:57:16 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -255,7 +255,7 @@ while True:
                 )
 
                 if next_minutes - now_minutes <= 3:
-                    state = f"あと3分以内で\n{next_item['name']}が始まります！"
+                    state = f"休み時間です/nあと3分以内で{next_item['name']}が始まります！"
 
                     break
 
