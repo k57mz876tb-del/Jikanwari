b794afa6fdeb2cf95bf175a7dfde0478123b756b
k57mz876tb-del <k57mz876tb@privaterelay.appleid.com>
2026-09-15 10:42:47 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -250,21 +250,7 @@ while True:
                 state = "休み時間です"
                 break
                 
-if "授業中" in state:
-    bg_color = CLASS_BG_COLOR
-    text_color = CLASS_TEXT_COLOR
 
-elif "休み時間" in state:
-    bg_color = BREAK_BG_COLOR
-    text_color = BREAK_TEXT_COLOR
-
-elif "昼休み" in state:
-    bg_color = LUNCH_BG_COLOR
-    text_color = LUNCH_TEXT_COLOR
-
-else:
-    bg_color = AFTER_SCHOOL_BG_COLOR
-    text_color = AFTER_SCHOOL_TEXT_COLOR
 
     
 st.write(state)
