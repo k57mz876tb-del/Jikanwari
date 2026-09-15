e6590a69f127cde80688ef9ec21e1c8021891ff0
k57mz876tb-del <k57mz876tb@privaterelay.appleid.com>
2026-09-15 11:02:08 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -174,7 +174,8 @@ already_done = set()
 # =========================
 
 #while True:
-    #kazuto = 0
+kazuto = 1
+
 while kazuto == 1:
     now = datetime.now(ZoneInfo("Asia/Tokyo")).time().strftime("%H:%M")
     #now = "14:12"
